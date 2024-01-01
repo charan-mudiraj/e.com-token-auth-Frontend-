af360c78d2edb1ab1bc6a960ffa6475ea8bbc669
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2024-01-01 13:57:19 +0530

    8

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,29 @@
 import "./About.css";
 import { useNavigate } from "react-router-dom";
+import ppImg from "../assets/images/pp.jpg";
+import twitterImg from "../assets/images/twitter.png";
+import linkedInImg from "../assets/images/linkedin.png";
+import instagramImg from "../assets/images/instagram.png";
+import whatsappImg from "../assets/images/whatsapp.png";
 
 export default function About() {
   const navigate = useNavigate();
   const dir = "../assets/images/";
   const icons = [
     {
-      imgPath: dir + "twitter.png",
+      imgPath: twitterImg,
       link: "https://twitter.com/_charan_mudiraj?t=FkppQqe4UIRLmsZdzomz7w&s=09",
     },
     {
-      imgPath: dir + "linkedin.png",
+      imgPath: linkedInImg,
       link: "https://www.linkedin.com/in/charan-mudiraj-868086230",
     },
     {
-      imgPath: dir + "instagram.png",
+      imgPath: instagramImg,
       link: "https://instagram.com/h9ck_berry?utm_source=qr&igshid=ZDc4ODBmNjlmNQ%3D%3D",
     },
     {
-      imgPath: dir + "whatsapp.png",
+      imgPath: whatsappImg,
       link: "https://wa.me/qr/H67AUKBDVVBUO1",
     },
   ];
@@ -34,7 +39,7 @@ export default function About() {
       <button onClick={() => navigate("/home")}>Go to Home</button>
       <div id="ac-about-card">
         <span>
-          <img src="../assets/pp.jpg" alt="" />
+          <img src={ppImg} alt="" />
         </span>
         <p>Admin</p>
         <p>Charan M</p>
